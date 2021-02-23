b56af0b5a2e8219677af42bd1fd7acdf807b5acf
unknown <sunruolun@gmail.com>
2021-02-23 14:25:51 -0500

    nav

---
+++ explomo/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import './NavBar.css';
+
+
+const NavBar = (props) => {
+    let nav = props.user ?
+      <div>
+        <Link to='/login' className='NavBar-link' onClick={props.handleLogout}>LOG OUT</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+        <span className='NavBar-welcome'>WELCOME, {props.user.name}</span>
+        <NavLink to='/entry-form' className='NavBar-link' activeClassName='selected'><strong>+</strong>Explore</NavLink>
+      </div>
+      :
+      <div>
+        <NavLink to='/login' className='NavBar-link' activeClassName="selected">LOG IN</NavLink>
+        &nbsp;&nbsp;|&nbsp;&nbsp;
+        <NavLink to='/signup' className='NavBar-link' activeClassName="selected">SIGN UP</NavLink>
+      </div>;
+
+    return (
+      <div className='navbar fixed-top custom-nav'>
+        {nav}
+      </div>
+    );
+};
+
+export default NavBar;
